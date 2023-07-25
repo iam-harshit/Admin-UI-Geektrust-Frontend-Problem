59f70127d6dbc3bb086d1a5146128c03e17c5d7c
iam-harshit <importantemail545@gmail.com>
2023-07-25 15:47:01 +0530

    Update search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 function Search(){
-    // flex bg-[url('./asset/search-icon.svg')] bg-no-repeat bg-[length:20px] p-6 relative bg-white rounded-2xl shadow-l
     return(
            <div className="searchbar-container">
                 <input type="search" name="user-input" id="input" placeholder="Search by name, email or role" required/>
