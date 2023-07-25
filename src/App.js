664cd58db1c8b68611dd6885e8b6f2104a0ccf3b
iam-harshit <importantemail545@gmail.com>
2023-07-25 16:16:19 +0530

    Install toast package and implement get API data logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import Table from './components/Table';
+import { useEffect, useState } from "react";
+import Table from "./components/Table";
+import {apiUrl} from "./data";
+import {toast} from "react-toastify";
 
 function App() {
+  const [tableData, setTableData] = useState(null);
+
+  //handling state of spinner
+  const [loading, setLoading] = useState(true);
+
+  function fetchData() {
+    setLoading(true);
+    try {
+      const response = fetch(apiUrl);
+      const output = response.json();
+      setTableData(output);
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
+    setLoading(false);
+  }
+
+  useEffect( () =>{
+    fetchData();
+  },[])
+
   return (
     <div className="app">
-      <Table/>
+      <Table tableData={tableData} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { ToastContainer } from "react-toastify";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <div>
     <App />
+    <ToastContainer />
+  </div>
 );
